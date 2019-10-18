68b301921e0c5edea5278d0e1c6f0188b79dcc64
Nirmalya Bandyopadhyay <nirmalya@broadinstitute.org>
2019-10-18 13:46:03 -0400

    Refactored code

--- R/DESeqGSMain.R
+++ R/DESeqGSMain.R
@@ -1,10 +1,3 @@
-#library(tidyverse)
-#library(googledrive)
-#library(googlesheets4)
-#library(DESeq2)
-#library(data.table)
-#library(docopt)
-
 # Function for getting value of a factor
 get_factor_val <- function(factor_str) {
     factor_val <- sub("@factor\\s+(\\S+)", "\\1", factor_str)
@@ -105,44 +98,7 @@ getCountData <- function(count_tab, colData, CDS_only) {
 }
 
 
-#drive_auth(email = TRUE)
-#sheets_auth(email = TRUE)
-
-DESeqGS <- function(gs_id) {
-
-
-    pat_str <- "(https://docs.google.com/spreadsheets/d/)*(\\w+?)(/edit.*)"
-    gs_id_f = sub(pat_str, "\\2", gs_id)
-    lval1 <- sheets_get(ss = gs_id_f)
-
-    # There should be four tabs: metadata, samples, groups, tests
-
-    lsheets1 <- lval1$sheets
-
-    metadata_sheet <- read_sheet(ss = gs_id_f, sheet = "metadata")
-    samples_sheet <- read_sheet(ss = gs_id_f, sheet = "samples")
-    groups_sheet <- read_sheet(ss = gs_id_f, sheet = "groups")
-    tests_sheet <- read_sheet(ss = gs_id_f, sheet = "tests")
-
-    # Get the metadata
-
-    count_file <- get_metadata_val(metadata_sheet, "count_file")
-    outdir <- get_metadata_val(metadata_sheet, "outdir")
-    CDS_only <- get_metadata_val(metadata_sheet, "CDS_only")
-    sep_tag = get_metadata_val(metadata_sheet, "sep")
-
-    sep_str <- NA
-    if (sep_tag == "tab") {
-        sep_str = "\t"
-    } else if (sep_tag == "comma") {
-        sep_str = ","
-    } else {
-        err_str <- paste0("Unknown sep: ", sep_tag)
-        stop(err_str)
-    }
-
-    # Now get information from samples_sheet
-
+exe_samples_sheet <- function(samples_sheet) {
     samples_sheet_nrow <- dim(samples_sheet)[1]
     samples_sheet_ncol <- dim(samples_sheet)[2]
     samples_sheet_cols <- colnames(samples_sheet)
@@ -190,30 +146,26 @@ DESeqGS <- function(gs_id) {
         }
     }
 
-    # Now load information from groups_sheet
-    # We shall have two maps here; for non_ref and for ref
+    ret_lst <- list("factor_map" = factor_map, "sample_map" = sample_map)
+    return (ret_lst)
+}
 
+
+exe_groups_sheet <- function(groups_sheet, factor_map, sample_map) {
     groups_sheet_nrow <- dim(groups_sheet)[1]
     groups_sheet_ncol <- dim(groups_sheet)[2]
 
-    ref_map <- list()
-    non_ref_map <- list()
     group_samples_map <- list()
 
     for (j in 1:groups_sheet_nrow) {
         lrow <- groups_sheet[j, ]
         lgroup_name <- lrow[["@group_name"]]
-        #print(lgroup_name)
 
         lnon_ref_str <- lrow[["@non_ref"]]
         lnon_ref_samples <- get_group_samples(lnon_ref_str, factor_map, sample_map)
-        non_ref_map[[lgroup_name]] <- lnon_ref_samples
-        #print(lnon_ref)
 
         lref_str <- lrow[["@ref"]]
         lref_samples <- get_group_samples(lref_str, factor_map, sample_map)
-        ref_map[[lgroup_name]] <- lref_samples
-
 
         condVec <- c(rep("ref", length(lref_samples)), 
             rep("non_ref", length(lnon_ref_samples)))
@@ -225,8 +177,60 @@ DESeqGS <- function(gs_id) {
         group_samples_map[[lgroup_name]] <- colData
     }
 
-    # Now process the tests_sheet
+    ret_lst <- lst("group_samples_map" = group_samples_map)
+    return (ret_lst)
 
+}
+
+
+exe_single_way_test <- function(ltest_name, ltest_desc, group_samples_map, count_tab, outdir, CDS_only) {
+    loutdir <- paste0(outdir, "/", ltest_name)
+    lprefix <- ltest_name
+
+    dir.create(file.path(loutdir), recursive = TRUE)
+    logfile <- paste0(loutdir, "/", lprefix, "_logfile.txt")
+    #print(logfile)
+
+    file.create(logfile)
+    sink(logfile)
+
+    print(ltest_name)
+    print(ltest_desc)
+    print("........")
+
+    # Build the test
+    colData <- getColData(ltest_desc, group_samples_map)
+    lcount_tab <- getCountData(count_tab, colData, CDS_only)
+
+    print(colData)
+    design_col <- colnames(colData)
+    colnames(colData) <- "condition"
+    
+    dds <- DESeqDataSetFromMatrix(countData = lcount_tab,
+        colData = colData,
+        design = ~ condition)
+
+    dds$condition <- relevel(dds$condition, ref="ref")
+    dds <- DESeq(dds)
+    res <- results(dds)
+    resOrdered <- res[order(res$padj),]
+    resOrdered2 = setDT(data.frame(resOrdered), keep.rownames = TRUE)[]
+    colnames(resOrdered2)[1] <- "Gene_id"
+
+    # Write DESeq2 results and MA plots
+    outfile = paste0(loutdir, "/", lprefix, ".tsv")
+    write.table(resOrdered2, outfile, sep = "\t", row.names = FALSE)
+
+    ma_pdf_file = paste0(loutdir, "/", lprefix, "_MA.pdf")
+    pdf(ma_pdf_file)
+    plotMA(res, ylim=c(-10,10), main = lprefix)
+    dev.off()
+
+    print("")
+    sink()
+}
+
+exe_tests_sheet <- function (tests_sheet, count_file, group_samples_map, outdir, CDS_only, sep_str) {
     tests_sheet_nrow <- dim(tests_sheet)[1]
     tests_sheet_ncol <- dim(tests_sheet)[2]
 
@@ -250,54 +254,59 @@ DESeqGS <- function(gs_id) {
         if ('no' == tolower(ldo_test)) {
         } else {
         
-        
-            loutdir <- paste0(outdir, "/", ltest_name)
-            lprefix <- ltest_name
-
-            dir.create(file.path(loutdir), recursive = TRUE)
-            logfile <- paste0(loutdir, "/", lprefix, "_logfile.txt")
-            #print(logfile)
-
-            file.create(logfile)
-            sink(logfile)
-
-            print(ltest_name)
-            print(ltest_desc)
-            print("........")
-
-            # Build the test
-            colData <- getColData(ltest_desc, group_samples_map)
-            lcount_tab <- getCountData(count_tab, colData, CDS_only)
-
-            print(colData)
-            design_col <- colnames(colData)
-            colnames(colData) <- "condition"
-            # 
-            dds <- DESeqDataSetFromMatrix(countData = lcount_tab,
-                colData = colData,
-                design = ~ condition)
-
-
-            dds$condition <- relevel(dds$condition, ref="ref")
-            dds <- DESeq(dds)
-            res <- results(dds)
-            resOrdered <- res[order(res$padj),]
-            resOrdered2 = setDT(data.frame(resOrdered), keep.rownames = TRUE)[]
-            colnames(resOrdered2)[1] <- "Gene_id"
-
-            # Write DESeq2 results and MA plots
-            outfile = paste0(loutdir, "/", lprefix, ".tsv")
-            write.table(resOrdered2, outfile, sep = "\t", row.names = FALSE)
-
-            ma_pdf_file = paste0(loutdir, "/", lprefix, "_MA.pdf")
-            pdf(ma_pdf_file)
-            plotMA(res, ylim=c(-10,10), main = lprefix)
-            dev.off()
-
-            print("")
-            sink()
+            exe_single_way_test(ltest_name, ltest_desc, group_samples_map, count_tab, outdir, CDS_only)    
         }
     }
+}
+
+DESeqGS <- function(gs_id) {
+
+    pat_str <- "(https://docs.google.com/spreadsheets/d/)*(\\w+?)(/edit.*)"
+    gs_id_f = sub(pat_str, "\\2", gs_id)
+    lval1 <- sheets_get(ss = gs_id_f)
+
+    # There should be four tabs: metadata, samples, groups, tests
+
+    lsheets1 <- lval1$sheets
+
+    metadata_sheet <- read_sheet(ss = gs_id_f, sheet = "metadata")
+    samples_sheet <- read_sheet(ss = gs_id_f, sheet = "samples")
+    groups_sheet <- read_sheet(ss = gs_id_f, sheet = "groups")
+    tests_sheet <- read_sheet(ss = gs_id_f, sheet = "tests")
+
+    # Get the metadata
+
+    count_file <- get_metadata_val(metadata_sheet, "count_file")
+    outdir <- get_metadata_val(metadata_sheet, "outdir")
+    CDS_only <- get_metadata_val(metadata_sheet, "CDS_only")
+    sep_tag = get_metadata_val(metadata_sheet, "sep")
+
+    sep_str <- NA
+    if (sep_tag == "tab") {
+        sep_str = "\t"
+    } else if (sep_tag == "comma") {
+        sep_str = ","
+    } else {
+        err_str <- paste0("Unknown sep: ", sep_tag)
+        stop(err_str)
+    }
+
+    # 1. Get information from samples_sheet
+
+    ret_val1 <- exe_samples_sheet(samples_sheet)
+    factor_map <- ret_val1$factor_map
+    sample_map <- ret_val1$sample_map
+
+    # 2. Load information from groups_sheet
+
+    ret_val2 <- exe_groups_sheet(groups_sheet, factor_map = factor_map, 
+        sample_map = sample_map)
+    group_samples_map <- ret_val2$group_samples_map
+
+    # 3. Process the tests_sheet
+    
+    exe_tests_sheet(tests_sheet, count_file, group_samples_map, outdir, CDS_only, sep_str)
+
 }
 
 
