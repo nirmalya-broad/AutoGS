8afbcef9d1fc112e1edb0172d1e794281cdfc78a
Nirmalya Bandyopadhyay <nirmalya@broadinstitute.org>
2019-11-20 16:49:25 -0500

    Added some more code

--- R/PCAMain.R
+++ R/PCAMain.R
@@ -1,6 +1,6 @@
 # Customized code for updating color, shape and size to the input plot.
 
-append_factor_to_df <- function(pca_df, aes_type_str, aes_sheet, map_factors) {
+append_factor <- function(pca_df, aes_type_str, aes_sheet, map_factors) {
 
     lrow1 <- aes_sheet[aes_sheet[, "@aes_type"] == aes_type_str,]
     row_count <- dim(lrow1)[1]
@@ -14,10 +14,109 @@ append_factor_to_df <- function(pca_df, aes_type_str, aes_sheet, map_factors) {
 }
 
 
-exe_aes_sheet <- function(aes_sheet, all_sample_names, factor_map, sample_map) {
+append_aes_map <- function(lplt1, laes_type, aes_types, aes_sheet) {
 
-    
+    lplt2 <- NA
+    if (laes_type == "color") {
+        lplt2 <- append_color_map(lplt1, aes_types, aes_sheet)
+    } else if (laes_type == "shape") {
+        lplt2 <- append_shape_map(lplt1, aes_types, aes_sheet)
+    } else if (laes_type == "size") {
+        lplt2 <- append_size_map(lplt1, aes_types, aes_sheet)
+    } else {
+        err_str <- paste0("Illegal aes_type", laes_type)
+        stop(err_str)
+    }
+
+    return (lplt2)
 }
+
+append_color_map <- function(lplt1, aes_types, aes_sheet) {
+
+    lplt2 <- NA
+    if ("color" %in% aes_types) {
+    
+        # Get the palette and create the aesthetic mapping
+        lrow1 <- aes_sheet[aes_sheet[, "@aes_type"] == "color", ]
+        
+        if (lrow1$`@aes_assign_proc` == "palette") {
+            # get the name of the palette
+            palette_val <- lrow1$`@aes_assign_value`
+
+            lplt2 <- lplt1 + scale_colour_brewer(palette = palette_val) 
+            
+        } else {
+            err_str <- paste0("aes color format not supported")
+            stop(err_str)
+        }   
+
+    } else {
+
+        lplt2 <- lplt1
+    }
+
+    return (lplt2)
+}
+
+append_shape_map <- function(lplt1, aes_types, aes_sheet) {
+
+    lplt2 <- NA
+    if ("shape" %in% aes_types) {
+
+        lrow1 <- aes_sheet[aes_sheet[, "@aes_type"] == "shape", ]
+        
+        if (lrow1$`@aes_assign_proc` == "list") {
+            # get the name of the palette
+            shape_val1 <- lrow1$`@aes_assign_value`
+            shape_val2 <- trim(shape_val1)
+            shape_vals <- strsplit(shape_val2, split = "\\s*,\\s*", perl = TRUE) 
+
+            lplt2 <- lplt1 + scale_shape_manual(values = shape_vals) 
+            
+        } else {
+            err_str <- paste0("aes color format not supported")
+            stop(err_str)
+        }   
+
+    } else {
+
+        lplt2 <- lplt1
+    }
+
+    return (lplt2)
+}
+
+append_size_map <- function(lplt1, aes_types, aes_sheet) {
+
+    lplt2 <- NA
+
+    if ("size" %in% aes_types) {
+
+        lrow1 <- aes_sheet[aes_sheet[, "@aes_type"] == "size", ]
+
+        if (lrow1$`@aes_assign_proc` == "list") {
+            # get the name of the palette
+            shape_val1 <- lrow1$`@aes_assign_value`
+            shape_val2 <- trim(shape_val1)
+            shape_vals <- strsplit(shape_val2, split = "\\s*,\\s*", perl = TRUE) 
+            lplt2 <- lplt1 + scale_shape_manual(values = shape_vals)
+            
+        } else {
+            err_str <- paste0("aes color format not supported")
+            stop(err_str)
+        }
+
+    } else {
+
+        lplt2 <- lplt1
+    }
+
+    return (lplt2)
+}
+    
+
+# From https://stackoverflow.com/a/2261149
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 DESeqGS <- function(gs_id) {
 
@@ -53,7 +152,8 @@ DESeqGS <- function(gs_id) {
     outdir <- get_metadata_val(metadata_sheet, "outdir")
     viz_type <- get_metadata_val(metadata_sheet, "viz_type")
     PCA_scale_str <- get_metadata_val(metadata_sheet, "PCA_scale")
-    sep_tag = get_metadata_val(metadata_sheet, "sep")
+    sep_tag <- get_metadata_val(metadata_sheet, "sep")
+    aes_types_str <- get_metadata_val(metadata_sheet, "aes_types")
 
     sep_str <- NA
     if (sep_tag == "tab") {
@@ -66,6 +166,10 @@ DESeqGS <- function(gs_id) {
     }
 
     PCA_scale <- as.logical(toupper(PCA_scale_str))
+
+    aes_types1 <- trim(aes_types_str)
+    aes_types2 <-  strsplit(aes_types1, split = "\\s*,\\s*", perl = TRUE)
+    aes_types <- aes_types2[[1]]
 
     count_tab <- read.csv(count_file, sep = sep_str, header = TRUE, 
         row.names = 1, stringsAsFactors = FALSE, check.names = FALSE)
@@ -89,16 +193,22 @@ DESeqGS <- function(gs_id) {
 
     # 4. Append factors
 
-    pca_df2 <- append_factor_to_df(pca_df1, "color", aes_sheet, map_factors)
-    pca_df3 <- append_factor_to_df(pca_df2, "shape", aes_sheet, map_factors)
-    pca_df <- append_factor_to_df(pca_df3, "size", aes_sheet, map_factors)
+    pca_df2 <- append_factor(pca_df1, "color", aes_sheet, map_factors)
+    pca_df3 <- append_factor(pca_df2, "shape", aes_sheet, map_factors)
+    pca_df <- append_factor(pca_df3, "size", aes_sheet, map_factors)
 
 
     # 5. Start building ggplot2
 
-    plt <- ggplot(pca_df, aes(x = PC1, y = PC2, color = color))
+    plt1 <- ggplot(pca_df, aes(x = PC1, y = PC2))
 
-    plt2 <- plt + geom_point(size = 2)
+    # 6. Add aesthetic mappings
+
+    pl2 <- append_aes_map(plt1, "shape", aes_types, aes_sheet)        
+    plt3 <- append_aes_map(plt2, "size", aes_types, aes_sheet)        
+    plt_f <- append_aes_map(plt3, "color", aes_types, aes_sheet)
+
+
 
     
 }
